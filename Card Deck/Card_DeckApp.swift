6bf54e378e9695f4d727f14d7286af7872f9548b
Greg Swan <gmswan99@gmail.com>
2023-11-08 15:43:29 -0500

    Initial Commit

---
+++ Card Deck/Card_DeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_DeckApp.swift
+//  Card Deck
+//
+//  Created by Greg Swan on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Card_DeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card Deck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card Deck
+//
+//  Created by Greg Swan on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
