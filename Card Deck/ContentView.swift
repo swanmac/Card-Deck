45982489a8a7e6822943f4ca9df0b3a690574d61
Greg Swan <gmswan99@gmail.com>
2023-11-08 15:48:48 -0500

    initial commit

--- Card Deck/ContentView.swift
+++ Card Deck/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Image("background")
+            
+            VStack {
+                Spacer()
+                Image("cdlogo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                    
+                }
+                Spacer()
+                
+                Button(action: {
+                    
+                    // Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if  cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                   
+                    
+                }, label: {
+                    Image("deal")
+                })
+                
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            
+            }
+            
         }
-        .padding()
+        
     }
 }
 
